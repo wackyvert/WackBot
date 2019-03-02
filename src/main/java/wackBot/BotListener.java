db5d266f4acace3826b3f17843a7ac24637dfa1a
Wackyvert <wvinc.com@gmail.com>
2019-03-01 19:08:40 -0600

    V1.03, fixed the onMessageReceived event so it doesn't respond to any bots (even though thats not the thing that was screwing it up in the first place ) because sam told me to (LETS TRY THIS ONE AGAIN I FUCKED THE FIRST COMMIT UP)

--- src/main/java/wackBot/BotListener.java
+++ src/main/java/wackBot/BotListener.java
@@ -13,7 +13,7 @@ public class BotListener {
     public void onMessageEvent(MessageReceivedEvent event) throws InterruptedException, IOException {
 
 
-        if (event.getMessage().getContent().toLowerCase().startsWith(prefix).isBot) ;
+        if (event.getMessage().getContent().toLowerCase().startsWith(prefix)) ;
         CommandProcessor.processCommand(event.getMessage(), prefix);
     }
 }
